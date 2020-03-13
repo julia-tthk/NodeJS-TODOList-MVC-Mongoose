10aab04fc47a827336f2affd1dcfc32157dacea3
Julia Aasmaa <my_e_10@hotmail.com>
2020-03-13 12:08:32 +0200

    v3

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs');
 
 
 const items = [];
+const itemsStudy = [];
 
 app.get('/', (req, res)=>{
     let today = new Date();
@@ -19,7 +20,7 @@ app.get('/', (req, res)=>{
     }
     
     let day = today.toLocaleDateString("en-US", options);
-    console.log(day);
+    //console.log(day);
 
     res.render('index', {
         listTitle: day,
@@ -28,16 +29,23 @@ app.get('/', (req, res)=>{
 });
 
 app.post('/', (req, res)=>{
+    console.log(req.body.list);
     let item = req.body.newItem;
-    items.push(item);
-    console.log(items);
-    res.redirect('/');
+    if(req.body.list === "Study TODO"){
+        itemsStudy.push(item);
+        res.redirect('/study');
+    } else {
+        items.push(item);
+        res.redirect('/');
+    }
+
+    
 });
 
 app.get('/study', (req, res) => {
     res.render('index', {
         listTitle: "Study TODO",
-        newListItems: ""
+        newListItems: itemsStudy
     })
 });
 
