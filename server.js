ffbcc289989062dd7660994b3ffc0ac7e789d5bc
Julia Aasmaa <my_e_10@hotmail.com>
2020-03-13 11:09:20 +0200

    v1

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+    let today = new Date();
+    //to get the date and the day of the week
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
+    
+    let day = today.toLocaleDateString("en-US", options);
+    console.log(day);
+
+    res.render('index', {
+        listTitle: day
+    })
+});
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000");    
+})
