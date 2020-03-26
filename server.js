76f7d6c637673257c2bc8caf40856ad70e0cd5f6
julia-vrn <julia.voronetskaja@gmail.com>
2020-03-26 22:33:48 +0200

    Add and Remove Items

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+mongoose.connect('mongodb://localhost:27017/todolistDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-const items = [];
-const itemsStudy = [];
+//create item schema
+const itemSchema = {
+    name: String
+};
+
+//create mongoose model, models are always in capital
+const Item = mongoose.model('Item', itemSchema);
+
+
+
+
+//const items = [];
+//const itemsStudy = [];
 
 app.get('/', (req, res)=>{
     let today = new Date();
@@ -22,24 +35,48 @@ app.get('/', (req, res)=>{
     let day = today.toLocaleDateString("en-US", options);
     //console.log(day);
 
-    res.render('index', {
-        listTitle: day,
-        newListItems: items
-    })
+    //{} find all the items inside the collection
+    Item.find({}, function(error, fetchedItems){
+        console.log(fetchedItems);
+        res.render('index', {
+            listTitle: day,
+            newListItems: fetchedItems
+        });
+    });
+
+    
 });
 
 app.post('/', (req, res)=>{
     console.log(req.body.list);
-    let item = req.body.newItem;
-    if(req.body.list === "Study TODO"){
+    let userInput = req.body.newItem;
+
+    //create a new document to store in the DB using the data we got from user
+    const item = new Item({
+        name: userInput
+    });
+
+    item.save();
+    res.redirect('/');
+    /*if(req.body.list === "Study TODO"){
+
         itemsStudy.push(item);
         res.redirect('/study');
     } else {
         items.push(item);
         res.redirect('/');
-    }
+    }*/  
+});
 
-    
+app.post('/delete', (req, res) => {
+    console.log(req.body.checkbox);
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId, function(error) {
+        if(!error){
+            console.log("Successfully deleted checked item!");
+            res.redirect('/');
+        }
+    });
 });
 
 app.get('/study', (req, res) => {
